3aa281bf9ad011ce5900b09d06de4c8b74dde374
Ataul Munim <hello@ataulm.com>
2019-10-09 17:31:35 +0100

    Add materialthemeoverlay for custom view

--- demo/src/main/java/com/example/DemoActivity.kt
+++ demo/src/main/java/com/example/DemoActivity.kt
@@ -1,7 +1,10 @@
 package com.example
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 import com.ataulm.mdcshowcase.startMdcShowcase
 
 class DemoActivity : AppCompatActivity() {
@@ -11,8 +14,17 @@ class DemoActivity : AppCompatActivity() {
         startMdcShowcase(
                 R.style.Theme_Demo,
                 shouldSetSupportActionBar = true,
-                inflatables = arrayListOf(R.layout.mdcs__inflatables_all)
+                inflatables = arrayListOf(R.layout.demo_inflatable_buttons)
         )
         finish()
     }
 }
+
+class AtaulmTextView(
+        ctx: Context,
+        attrs: AttributeSet
+) : AppCompatTextView(
+        ThemeEnforcement.createThemedContext(ctx, attrs, R.attr.ataulmTextViewStyle, 0),
+        attrs,
+        R.attr.ataulmTextViewStyle
+)
